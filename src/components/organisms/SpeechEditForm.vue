555cffac637f1496dd2708ecbb435d3fd821367e
yahiro <yahiro07@users.noreply.github.com>
2024-02-12 08:44:49 +0900

    コンポーネントを分離

---
+++ src/components/organisms/SpeechEditForm.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { type UserInfo } from '@/common/applicationData'
+import { textCaps } from '@/common/constants'
+import ButtonPrimary from '@/components/atoms/ButtonPrimary.vue'
+import BlockUserInfoPart from '@/components/molecules/BlockUserInfoPart.vue'
+import { checkStringLength } from '@/utils/textHelper'
+import { computed, ref } from 'vue'
+
+defineProps<{ user: UserInfo }>()
+
+const emit = defineEmits<{
+  (e: 'submit', contentText: string): void
+}>()
+
+const contentText = ref('')
+
+const canSubmit = computed(() => checkStringLength(contentText.value, 1, textCaps.blockContentText))
+
+const lengthsText = computed(() => `${contentText.value.length} / ${textCaps.blockContentText}`)
+
+function submitComment() {
+  if (canSubmit.value) {
+    emit('submit', contentText.value)
+    contentText.value = ''
+  }
+}
+</script>
+
+<template>
+  <div class="fc-speech-edit-form">
+    <div class="header-row">
+      <BlockUserInfoPart :user="user" class="user-info-part" />
+      <span class="lengths"> {{ lengthsText }} </span>
+      <ButtonPrimary text="投稿" @click="submitComment" :disabled="!canSubmit" />
+    </div>
+    <div class="content-row">
+      <textarea placeholder="コメントを追加" v-model="contentText" />
+    </div>
+    <!-- ボタンを下に表示するパターン -->
+    <div class="bottom-row" v-if="false">
+      <ButtonPrimary text="投稿" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.fc-speech-edit-form {
+  background: var(--cl-white);
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: var(--spec-shadow-light);
+  > .header-row {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+
+    > .user-info-part {
+      margin-right: auto;
+    }
+    > .lengths {
+      color: var(--cl-gray-2);
+    }
+  }
+
+  > .content-row {
+    margin-top: 4px;
+    display: flex;
+    align-items: flex-start;
+    gap: 4px;
+
+    > textarea {
+      flex-grow: 1;
+      resize: none;
+      min-height: 100px;
+      padding: 8px 4px;
+      border: none;
+      outline: none;
+      border-bottom: solid 1px var(--cl-light-gray-2);
+      &::placeholder {
+        color: var(--cl-gray-1);
+      }
+    }
+  }
+  > .bottom-row {
+    margin-top: 8px;
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/features/NoteEditor.vue
+++ src/features/NoteEditor.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
 import { exampleNote1, exampleUserInfo } from '@/common/applicationData'
-import { textCaps } from '@/common/constants'
-import ButtonPrimary from '@/components/atoms/ButtonPrimary.vue'
-import BlockUserInfoPart from '@/components/molecules/BlockUserInfoPart.vue'
 import SpeechCard from '@/components/organisms/SpeechCard.vue'
+import SpeechEditForm from '@/components/organisms/SpeechEditForm.vue'
 import { generateIdTimeSequential } from '@/utils/idGenerator'
-import { checkStringLength } from '@/utils/textHelper'
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 
 const note = ref(exampleNote1)
 const user = exampleUserInfo
 
-const contentText = ref('')
-
-const canSubmit = computed(() => checkStringLength(contentText.value, 1, textCaps.blockContentText))
-
-const lengthsText = computed(() => `${contentText.value.length} / ${textCaps.blockContentText}`)
-
-function submitComment() {
-  if (canSubmit.value) {
-    note.value.speeches.push({
-      id: generateIdTimeSequential(),
-      contentText: contentText.value
-    })
-    contentText.value = ''
-  }
+function addComment(contentText: string) {
+  note.value.speeches.push({
+    id: generateIdTimeSequential(),
+    contentText: contentText
+  })
 }
 </script>
 
@@ -33,20 +21,7 @@ function submitComment() {
     <div class="blocks">
       <SpeechCard v-for="item of note.speeches" :key="item.id" :speech="item" :user="user" />
     </div>
-    <div class="edit-area">
-      <div class="header-row">
-        <BlockUserInfoPart :user="user" class="user-info-part" />
-        <span class="lengths"> {{ lengthsText }} </span>
-        <ButtonPrimary text="投稿" @click="submitComment" :disabled="!canSubmit" />
-      </div>
-      <div class="content-row">
-        <textarea placeholder="コメントを追加" v-model="contentText" />
-      </div>
-      <!-- ボタンを下に表示するパターン -->
-      <div class="bottom-row" v-if="false">
-        <ButtonPrimary text="投稿" />
-      </div>
-    </div>
+    <SpeechEditForm :user="user" @submit="addComment" />
   </div>
 </template>
 
@@ -60,51 +35,6 @@ function submitComment() {
     display: flex;
     flex-direction: column;
     gap: 16px;
-  }
-
-  > .edit-area {
-    background: var(--cl-white);
-    padding: 16px;
-    display: flex;
-    flex-direction: column;
-    box-shadow: var(--spec-shadow-light);
-    > .header-row {
-      display: flex;
-      align-items: center;
-      gap: 16px;
-
-      > .user-info-part {
-        margin-right: auto;
-      }
-      > .lengths {
-        color: var(--cl-gray-2);
-      }
-    }
-
-    > .content-row {
-      margin-top: 4px;
-      display: flex;
-      align-items: flex-start;
-      gap: 4px;
-
-      > textarea {
-        flex-grow: 1;
-        resize: none;
-        min-height: 100px;
-        padding: 8px 4px;
-        border: none;
-        outline: none;
-        border-bottom: solid 1px var(--cl-light-gray-2);
-        &::placeholder {
-          color: var(--cl-gray-1);
-        }
-      }
-    }
-    > .bottom-row {
-      margin-top: 8px;
-      display: flex;
-      justify-content: flex-end;
-    }
   }
 }
 </style>
